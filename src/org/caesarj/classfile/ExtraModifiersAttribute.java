b41fd9858ce47e663c7e0d90a36f0c1423bccaa9
klose <klose>
2004-04-07 09:20:56 +0000

    added

---
+++ src/org/caesarj/classfile/ExtraModifiersAttribute.java
@@ -0,0 +1,71 @@
+/*
+ * Created on 16.03.2004
+ *
+ */
+package org.caesarj.classfile;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * This class represents an generic attribute that stores the extra modifiers 
+ * (caesar modifiers that are not supported by the VMS format) of a class in a 
+ * classfile. The name of this attribute is <code>org.caesarj.compiler.ExtraModifiers</code>.
+ * 
+ * @author Karl Klose
+ */
+public class ExtraModifiersAttribute extends GenericAttribute {
+	
+	/** Name constants */
+	public final static String	AttributePrefix = "org.caesarj.compiler.";
+	public final static String	AttributeName  = AttributePrefix + "ExtraModifiers";
+	
+	/** The binary mask for the modifiers to store */
+	public final static int		EXTRA_MOD_MASK	= ~0xFFF;
+
+	/**
+	 * Create a new attribute
+	 * @param modifiers	The class' modifier
+	 */
+	public ExtraModifiersAttribute(int modifiers) {
+		super(AttributeName, modifiersToByteArray(modifiers));
+		extraModifiers = modifiers & EXTRA_MOD_MASK;
+	}
+	
+	/**
+	 * Read from a input stream
+	 * @param in	The input stream
+	 * @param cp	The constant pool to be used
+	 */
+	public ExtraModifiersAttribute(DataInput in, ConstantPool cp) throws IOException {
+		super( new AsciiConstant(AttributeName), in, cp );
+		extraModifiers = byteArrayToModifiers( getData() );
+	}
+
+	/**
+	 * Converts an integer to a byte array representation to be stored
+	 */
+	static byte[] modifiersToByteArray( int modifiers ){
+		int extraModifiers = modifiers & EXTRA_MOD_MASK;
+		byte[] data = String.valueOf(extraModifiers).getBytes();
+		return data;
+	}
+	
+	/**
+	 * Converts a byte array back to an integer
+	 */
+	static int byteArrayToModifiers( byte[] data ){
+		String s = new String(data);
+		return Integer.parseInt(s);
+	}
+	
+	/**
+	 * Get the attributes value as integer
+	 * @return The stored extra modifiers
+	 */
+	public int getExtraModifiers(){
+		return extraModifiers;
+	}
+	private int extraModifiers;
+}
