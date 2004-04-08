376d777e2970cb96d6e686b99d11d67eca53e4b5
aracic <aracic>
2004-04-08 15:55:43 +0000

    *** empty log message ***

--- test/VirtualClassesTests/VCTestCase_0.java
+++ test/VirtualClassesTests/VCTestCase_0.java
@@ -14,17 +14,17 @@ public class VCTestCase_0 extends TestCase {
 
 	public void test() {
         B b = new B();
-        A a = b;        
+        A a = b;
         D d = new D();
         
         System.out.println(a.a());
         System.out.println(b.a());
         System.out.println(b.b());
 
-        System.out.println(d.a());        
+        System.out.println(d.a());
         System.out.println(d.b());
         System.out.println(d.c());
-        System.out.println(d.d());   
+        System.out.println(d.d());
 	}
 }
 
@@ -48,12 +48,17 @@ public cclass C extends A {
 
 public cclass D extends B & C {
     public String d() {
-        return "";
-        //return a()+b()+c();
+        return a()+'-'+b()+'-'+c();
     }
     
     // CTODO: this one is manually added, in order to get compiler work in current state
     public String c() {
-        return "";
+        return "C.c";
+    }
+}
+
+public class X {
+    public String toString() {
+        return "X";
     }
 }
