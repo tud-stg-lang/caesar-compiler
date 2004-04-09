4d2e528d0d507c657160c739cf223a7e17083728
aracic <aracic>
2004-04-09 15:56:39 +0000

    isEnabled check added

--- src/org/caesarj/compiler/ast/phylum/declaration/JInterfaceDeclaration.java
+++ src/org/caesarj/compiler/ast/phylum/declaration/JInterfaceDeclaration.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JInterfaceDeclaration.java,v 1.1 2004-03-15 11:56:48 aracic Exp $
+ * $Id: JInterfaceDeclaration.java,v 1.2 2004-04-09 15:56:39 aracic Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.declaration;
@@ -111,6 +111,9 @@ public class JInterfaceDeclaration extends JTypeDeclaration {
    * @exception	PositionedError	an error with reference to the source file
    */
   public void checkInterface(CContext context) throws PositionedError {
+      // IVICA
+      if(!isEnabled()) return;
+
     checkModifiers(context);
 
     statInit = constructStaticInitializers(context);
@@ -176,6 +179,10 @@ public class JInterfaceDeclaration extends JTypeDeclaration {
    * @exception	PositionedError	an error with reference to the source file
    */
   public void checkInitializers(CContext context) throws PositionedError {
+     
+    // IVICA
+    if(!isEnabled()) return;
+      
     //    CInterfaceContext self = new CInterfaceContext(context, context.getEnvironment(), sourceClass, this);
     //Walter start
     //self = new CInterfaceContext(context, context.getEnvironment(), sourceClass, this);
@@ -201,6 +208,9 @@ public class JInterfaceDeclaration extends JTypeDeclaration {
    * @exception	PositionedError	an error with reference to the source file
    */
   public void checkTypeBody(CContext context) throws PositionedError {
+      // IVICA
+      if(!isEnabled()) return;
+    
     //    CInterfaceContext self = new CInterfaceContext(context, context.getEnvironment(), sourceClass, this);
 
     for (int i = 0; i < inners.length; i++) {
