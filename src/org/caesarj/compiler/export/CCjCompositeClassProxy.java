1b9c63797aa9f138b4cee45307c05b6ed53c383e
aracic <aracic>
2004-04-16 14:29:31 +0000

    descendsFrom overridden to work with interfaces

--- src/org/caesarj/compiler/export/CCjCompositeClassProxy.java
+++ src/org/caesarj/compiler/export/CCjCompositeClassProxy.java
@@ -1,5 +1,10 @@
 package org.caesarj.compiler.export;
 
+import java.util.ArrayList;
+
+import org.caesarj.compiler.types.CCompositeType;
+import org.caesarj.compiler.types.CReferenceType;
+
 /**
  * ...
  * 
@@ -7,16 +12,41 @@ package org.caesarj.compiler.export;
  */
 public class CCjCompositeClassProxy extends CBadClass {
 
-    // ----------------------------------------------------------------------
-    // CONSTRUCTORS
-    // ----------------------------------------------------------------------
+    private CCompositeType compositeType;
 
-    public CCjCompositeClassProxy(String qualifiedName) {
+    public CCjCompositeClassProxy(CCompositeType compositeType, String qualifiedName) {
         super(qualifiedName);
+        this.compositeType = compositeType;
     }
 
     public boolean isAccessible(CClass from) {
         return true;
+    }   
+    
+	public boolean descendsFrom(CReferenceType from, CReferenceType[] actuals, CReferenceType[] substitution) {            		
+        CReferenceType[] interfaces = compositeType.getInterfaceList();
+        for(int i=0; i<interfaces.length; i++)
+            if(interfaces[i].getCClass().descendsFrom(from, actuals, substitution))
+                return true;
+        return false;
+	}
+
+    protected boolean descendsFrom(CClass from, ArrayList history) {
+        CReferenceType[] interfaces = compositeType.getInterfaceList();
+        for(int i=0; i<interfaces.length; i++)
+            if(interfaces[i].getCClass().descendsFrom(from, history))
+                return true;
+        return false;
     }
+    
+    public boolean descendsFrom(CClass from) {
+        CReferenceType[] interfaces = compositeType.getInterfaceList();
+        for(int i=0; i<interfaces.length; i++)
+            if(interfaces[i].getCClass().descendsFrom(from))
+                return true;
+        return false;
+	}
+
+    
 
 }
