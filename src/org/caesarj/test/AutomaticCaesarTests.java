ded9e51fd2201f7457e2412c4da8459587e59a9e
gasiunas <gasiunas>
2004-07-22 07:36:52 +0000

    doGeneratedTest renamed to compileAndRun test7 and test9 compile several files

--- src/org/caesarj/test/AutomaticCaesarTests.java
+++ src/org/caesarj/test/AutomaticCaesarTests.java
@@ -7,53 +7,65 @@ public class AutomaticCaesarTests extends FjTestCase
 	}
 	
 	public void testCaesarTestCase_0() throws Throwable {
-		doGeneratedTest("CaesarTestCase_0");
+		compileAndRun("CaesarTestCase_0");
 	}
 
 	public void testCaesarTestCase_1() throws Throwable {
-		doGeneratedTest("CaesarTestCase_1");
+		compileAndRun("CaesarTestCase_1");
 	}
 
 	public void testCaesarTestCase_2() throws Throwable {
-		doGeneratedTest("CaesarTestCase_2");
+		compileAndRun("CaesarTestCase_2");
 	}
 
 	public void testCaesarTestCase_3() throws Throwable {
-		doGeneratedTest("CaesarTestCase_3");
+		compileAndRun("CaesarTestCase_3");
 	}
 
 	public void testCaesarTestCase_4() throws Throwable {
-		doGeneratedTest("CaesarTestCase_4");
+		compileAndRun("CaesarTestCase_4");
 	}
 
 	public void testCaesarTestCase_5() throws Throwable {
-		doGeneratedTest("CaesarTestCase_5");
+		compileAndRun("CaesarTestCase_5");
 	}
 
 	public void testCaesarTestCase_6() throws Throwable {
-		doGeneratedTest("CaesarTestCase_6");
+		compileAndRun("CaesarTestCase_6");
 	}
 
 	public void testCaesarTestCase_7() throws Throwable {
+		removeClassFiles(null);
+		
+		compileFile("Barrier");
+		compileFile("CaesarTestCase_7");
+				
 		doGeneratedTest("CaesarTestCase_7");
 	}
-/*
-	public void testCaesarTestCase_8() throws Throwable {
-		doGeneratedTest("CaesarTestCase_8");
-	}
-*/
+
+//  tests priveleged access - the functionality not implemented
+//	public void testCaesarTestCase_8() throws Throwable {
+//		compileAndRun("CaesarTestCase_8");
+//	}
+
 	public void testCaesarTestCase_9() throws Throwable {
+		removeClassFiles(null);
+		
+		compileFile("Barrier");
+		compileFile("CaesarTestCase_9");
+				
 		doGeneratedTest("CaesarTestCase_9");
 	}
-      	
+     	
 	public void testCaesarTestCase_10() throws Throwable {
-		doGeneratedTest("CaesarTestCase_10");
+		compileAndRun("CaesarTestCase_10");
 	}
 	
 	public void testCaesarTestCase_11() throws Throwable {
-		doGeneratedTest("CaesarTestCase_11");
+		compileAndRun("CaesarTestCase_11");
 	}
+
 	public void testCaesarTestCase_12() throws Throwable {
-		doGeneratedTest("CaesarTestCase_12");
-	}	
+		compileAndRun("CaesarTestCase_12");
+	}
 }
