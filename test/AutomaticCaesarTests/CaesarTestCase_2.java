edcad73dc7f663db917edee4cc09f7652fc0f096
gasiunas <gasiunas>
2004-07-22 05:55:11 +0000

    Adjusted for syntax of virtual classes and current limitations

--- test/AutomaticCaesarTests/CaesarTestCase_2.java
+++ test/AutomaticCaesarTests/CaesarTestCase_2.java
@@ -7,14 +7,14 @@ public class CaesarTestCase_2 extends TestCase
 
 	public CaesarTestCase_2()
 	{
-		super("test");
+		super("bar"); // cannot dynamically call "test", because such call is not recognized by weaver
 	}
 
 	public static StringBuffer result = new StringBuffer();
 
 	public String expectedResult =
-		//":before foo:before exec foo:before exec foo:before exec foo:foo:after exec foo:after exec foo";
-	   	":before foo:foo";
+		":before call foo:before exec foo:before exec foo:before exec foo:foo:after exec foo:after exec foo:" + // first foo
+		"before call foo:before exec foo:before exec foo:foo:after exec foo"; 									// second foo
 
     public void bar()
     {
@@ -23,13 +23,13 @@ public class CaesarTestCase_2 extends TestCase
 
 	public void test()
 	{
-/*
-		deploy(new Aspect_2_Sub())
+		deploy(new Aspect_2_Sub_Impl(null))
 		{
 			foo();
 		}
-*/
+
 		foo();
+
 		System.out.println(result.toString());
 		assertEquals(expectedResult, result.toString());
 	}
@@ -42,37 +42,43 @@ public class CaesarTestCase_2 extends TestCase
 
 deployed cclass StaticAspect_2 {
 
-	pointcut callFoo() : /*cflow(call(* CaesarTestCase_2+.test())) && */call(* CaesarTestCase_2.foo());
+	pointcut callFoo() : cflow(call(* CaesarTestCase_2.test())) && call(* CaesarTestCase_2.foo());
 
-	before() : callFoo() {
-		CaesarTestCase_2.result.append(":before foo");
+	before() : callFoo()
+	{
+		CaesarTestCase_2.result.append(":before call foo");
 	}
 
 }
-/*
-deployed cclass StaticAspect_2_Sub extends StaticAspect_2 {
-	after() : callFoo() {
-		System.out.println("Hallo");
+
+/* inheritance from statically deployed classes is not supported
+deployed cclass StaticAspect_2_Sub extends StaticAspect_2
+{
+	after() : callFoo()
+	{
+		CaesarTestCase_2.result.append(":after foo");
 	}
 }
+*/
 
-crosscutting cclass Aspect_2 {
-
-	public final static deployed Aspect_2 THIS = new Aspect_2();
+cclass Aspect_2
+{
+	public final static deployed Aspect_2 THIS = new Aspect_2_Impl(null);
 
 	pointcut execFoo() : execution(* CaesarTestCase_2.foo());
 
-	before() : execFoo() {
+	before() : execFoo()
+	{
 		CaesarTestCase_2.result.append(":before exec foo");
 	}
 }
 
-crosscutting cclass Aspect_2_Sub extends Aspect_2 {
+cclass Aspect_2_Sub extends Aspect_2
+{
+	public final static deployed Aspect_2_Sub THIS = new Aspect_2_Sub_Impl(null);
 
-	public final static deployed Aspect_2_Sub THIS = new Aspect_2_Sub();
-
-	after() : execFoo() {
+	after() : execFoo()
+	{
 		CaesarTestCase_2.result.append(":after exec foo");
 	}
 }
-*/
