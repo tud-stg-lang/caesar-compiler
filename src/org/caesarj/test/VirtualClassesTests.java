3b5f59eefb82260bf4f06899f8ecbccadb9948dd
gasiunas <gasiunas>
2004-07-22 07:35:16 +0000

    test31 added doGeneratedTest renamed to compileAndRun

--- src/org/caesarj/test/VirtualClassesTests.java
+++ src/org/caesarj/test/VirtualClassesTests.java
@@ -7,135 +7,138 @@ public class VirtualClassesTests extends FjTestCase {
 	}
 
 	public void testCaesarTestCase_0() throws Throwable {
-        doGeneratedTest("test0", "VCTestCase");
+        compileAndRun("test0", "VCTestCase");
     }
 
     public void testCaesarTestCase_1() throws Throwable {
-        doGeneratedTest("test1", "VCTestCase");
+        compileAndRun("test1", "VCTestCase");
     }
 
     public void testCaesarTestCase_2() throws Throwable {
-        doGeneratedTest("test2", "VCTestCase");
+        compileAndRun("test2", "VCTestCase");
     }
 
     public void testCaesarTestCase_3() throws Throwable {
-        doGeneratedTest("test3", "VCTestCase");
+        compileAndRun("test3", "VCTestCase");
     }
 
     public void testCaesarTestCase_4() throws Throwable {
-        doGeneratedTest("test4", "VCTestCase");
+        compileAndRun("test4", "VCTestCase");
     }
     
     public void testCaesarTestCase_5() throws Throwable {
-        doGeneratedTest("test5", "VCTestCase");
+        compileAndRun("test5", "VCTestCase");
     }
  
    public void testCaesarTestCase_6() throws Throwable {
-        doGeneratedTest("test6", "VCTestCase");
+        compileAndRun("test6", "VCTestCase");
     }
 
     public void testCaesarTestCase_7() throws Throwable {
-        doGeneratedTest("test7", "VCTestCase");
+        compileAndRun("test7", "VCTestCase");
     }
 
 	public void testCaesarTestCase_8() throws Throwable {
-        doGeneratedTest("test8", "VCTestCase");
+        compileAndRun("test8", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_9() throws Throwable {
-        doGeneratedTest("test9", "VCTestCase");
+        compileAndRun("test9", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_10() throws Throwable {
-        doGeneratedTest("test10", "VCTestCase");
+        compileAndRun("test10", "VCTestCase");
     }
 
 	public void testCaesarTestCase_11() throws Throwable {
-        doGeneratedTest("test11", "VCTestCase");
+        compileAndRun("test11", "VCTestCase");
     }
 
 	public void testCaesarTestCase_12() throws Throwable {
-        doGeneratedTest("test12", "VCTestCase");
+        compileAndRun("test12", "VCTestCase");
     }
 
 	public void testCaesarTestCase_13() throws Throwable {
-        doGeneratedTest("test13", "VCTestCase");
+        compileAndRun("test13", "VCTestCase");
     }
 
 	public void testCaesarTestCase_14() throws Throwable {
-        doGeneratedTest("test14", "VCTestCase");
+        compileAndRun("test14", "VCTestCase");
     }
 
-	public void testCaesarTestCase_14b() throws Throwable {
-        doGeneratedTest("test14b", "VCTestCase");
+    public void testCaesarTestCase_14b() throws Throwable {
+        compileAndRun("test14b", "VCTestCase");
     }
 
 	public void testCaesarTestCase_15() throws Throwable {
-        doGeneratedTest("test15", "VCTestCase");
+        compileAndRun("test15", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_16() throws Throwable {
-        doGeneratedTest("test16", "VCTestCase");
+        compileAndRun("test16", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_17() throws Throwable {
-        doGeneratedTest("test17", "VCTestCase");
+        compileAndRun("test17", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_18() throws Throwable {
-        doGeneratedTest("test18", "VCTestCase");
+        compileAndRun("test18", "VCTestCase");
     }
 
 	public void testCaesarTestCase_19() throws Throwable {
-        doGeneratedTest("test19", "VCTestCase");
+        compileAndRun("test19", "VCTestCase");
     }
 
 	public void testCaesarTestCase_20() throws Throwable {
-        doGeneratedTest("test20", "VCTestCase");
+        compileAndRun("test20", "VCTestCase");
     }
     
     public void testCaesarTestCase_21() throws Throwable {
-        doGeneratedTest("test21", "VCTestCase");
+        compileAndRun("test21", "VCTestCase");
     }    
 	
 	public void testCaesarTestCase_22() throws Throwable {
-        doGeneratedTest("test22", "VCTestCase");
+        compileAndRun("test22", "VCTestCase");
     }    
     	
 	public void testCaesarTestCase_23() throws Throwable {
-        doGeneratedTest("test23", "VCTestCase");
+        compileAndRun("test23", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_24() throws Throwable {
-        doGeneratedTest("test24", "VCTestCase");
+        compileAndRun("test24", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_25() throws Throwable {
-        doGeneratedTest("test25", "VCTestCase");
+        compileAndRun("test25", "VCTestCase");
     }
 
 	public void testCaesarTestCase_26() throws Throwable {
-        doGeneratedTest("test26", "VCTestCase");
+        compileAndRun("test26", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_27() throws Throwable {
-        doGeneratedTest("test27", "VCTestCase");
+        compileAndRun("test27", "VCTestCase");
     }
 
 	public void testCaesarTestCase_28() throws Throwable {
-        doGeneratedTest("test28", "VCTestCase");
+        compileAndRun("test28", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_29() throws Throwable {
-        doGeneratedTest("test29", "VCTestCase");
+        compileAndRun("test29", "VCTestCase");
     }
 
 	public void testCaesarTestCase_30() throws Throwable {
-        doGeneratedTest("test30", "VCTestCase");
+        compileAndRun("test30", "VCTestCase");
+    }
+    
+    public void testCaesarTestCase_31() throws Throwable {
+        compileAndRun("test31", "VCTestCase");
     }
 	
 	public void testCaesarTestCase_99() throws Throwable {
-        doGeneratedTest("test99", "VCTestCase");
+        compileAndRun("test99", "VCTestCase");
     }
-
 }
