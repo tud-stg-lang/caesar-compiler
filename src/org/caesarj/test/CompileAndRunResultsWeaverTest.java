c4a1eee950ed1339a0883cb8cb04abcf8e055211
gasiunas <gasiunas>
2004-08-04 11:06:42 +0000

    Removed unnecessary imports

--- src/org/caesarj/test/CompileAndRunResultsWeaverTest.java
+++ src/org/caesarj/test/CompileAndRunResultsWeaverTest.java
@@ -1,17 +1,5 @@
 package org.caesarj.test;
 
-import java.io.PrintWriter;
-import java.util.Hashtable;
-import java.util.Vector;
-
-import org.caesarj.compiler.*;
-import org.caesarj.compiler.ast.phylum.JCompilationUnit;
-import org.caesarj.compiler.export.CSourceClass;
-import org.caesarj.compiler.types.KjcSignatureParser;
-import org.caesarj.compiler.types.KjcTypeFactory;
-import org.caesarj.compiler.types.SignatureParser;
-import org.caesarj.tools.antlr.runtime.ParserException;
-
 public class CompileAndRunResultsWeaverTest extends FjTestCase 
 {
 	public CompileAndRunResultsWeaverTest(String name)	{
