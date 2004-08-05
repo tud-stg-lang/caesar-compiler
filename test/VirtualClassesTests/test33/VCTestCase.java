d49acd0252c6d4f498b0c0323a8cf62e220501c8
gasiunas <gasiunas>
2004-08-05 08:00:32 +0000

    new test

---
+++ test/VirtualClassesTests/test33/VCTestCase.java
@@ -0,0 +1,68 @@
+package generated.test33;
+
+import junit.framework.*;
+import java.util.*;
+
+/**
+ * Test inheritance from implicit type.
+ *
+ * @author Vaidas Gasiunas
+ */
+public class VCTestCase extends TestCase
+{
+
+	public VCTestCase()
+	{
+		super("test");
+	}
+
+	public static final String expectedResult = "A.A.A, B.B.B, C.A.C";
+
+
+	public void test()
+	{
+		System.out.println("-------> VCTest 33: Inheritance from Implicit Type: start");
+        
+        OuterC.InnerB cb = (OuterC.InnerB)new OuterC_Impl(null).$newInnerB();
+	
+		String result = cb.queryA() + ", " + cb.queryB() + ", " + cb.queryC();
+
+		System.out.println(result);
+		assertEquals(expectedResult, result);
+
+        System.out.println("-------> VCTest 33: end");
+	}
+}
+
+public cclass OuterA
+{
+	public cclass InnerA
+	{
+		public String queryA()
+		{
+			return "A.A.A";
+		}
+	}
+}
+
+public cclass OuterB extends OuterA
+{
+    public cclass InnerB extends InnerA
+	{
+	    public String queryB()
+		{
+			return "B.B.B";
+		}
+	}
+}
+
+public cclass OuterC extends OuterB  
+{  
+    public cclass InnerA
+	{
+	    public String queryC()
+		{
+			return "C.A.C";
+		}
+	}
+}
