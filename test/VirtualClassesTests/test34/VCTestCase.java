796fd36e9ca262806f137350d445d1419f3b9e13
gasiunas <gasiunas>
2004-08-05 16:17:51 +0000

    New test

---
+++ test/VirtualClassesTests/test34/VCTestCase.java
@@ -0,0 +1,76 @@
+package generated.test34;
+
+import junit.framework.*;
+import java.util.*;
+
+/**
+ * Test mixin factory methods.
+ *
+ * @author Vaidas Gasiunas
+ */
+public class VCTestCase extends TestCase
+{
+
+	public VCTestCase()
+	{
+		super("test");
+	}
+
+	public static final String expectedResult = "B.B.B, C.C.C";
+
+
+	public void test()
+	{
+		System.out.println("-------> VCTest 34: Mixin factory methods: start");
+
+        OuterB.InnerB db = new OuterD_Impl(null).newB();
+        OuterC.InnerC dc = new OuterD_Impl(null).newC();
+
+		String result = db.queryB() + ", " + dc.queryC();
+
+		System.out.println(result);
+		assertEquals(expectedResult, result);
+
+        System.out.println("-------> VCTest 34: end");
+	}
+}
+
+public cclass OuterA
+{
+}
+
+public cclass OuterB extends OuterA
+{
+    public cclass InnerB
+	{
+	    public String queryB()
+		{
+			return "B.B.B";
+		}
+	}
+
+	public OuterB.InnerB newB()
+	{
+		return $newInnerB();
+	}
+}
+
+public cclass OuterC extends OuterA
+{
+    public cclass InnerC
+	{
+	    public String queryC()
+		{
+			return "C.C.C";
+		}
+	}
+
+	public OuterC.InnerC newC()
+	{
+		return $newInnerC();
+	}
+}
+
+public cclass OuterD extends OuterB & OuterC
+{
+}
