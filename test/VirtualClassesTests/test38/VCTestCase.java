c6d0794cb507feed7ae91db05b57bc44c5f9b30f
aracic <aracic>
2004-09-10 22:44:08 +0000

    added wrapper test

---
+++ test/VirtualClassesTests/test38/VCTestCase.java
@@ -0,0 +1,39 @@
+package generated.test38;
+
+import junit.framework.*;
+import java.util.*;
+
+/**
+ * Wrapper test
+ *
+ * @author Ivica Aracic
+ */
+public class VCTestCase extends TestCase {
+
+	public VCTestCase() {
+		super("test");
+	}
+
+	public void test() {
+		System.out.println("-------> VCTest 38: Wrapper test: start");
+
+        // just compile for now
+
+        System.out.println("-------> VCTest 38: end");
+	}
+}
+
+public class Client {
+}
+
+public cclass X {
+	public cclass W wraps Client {
+		public void print() {		
+			System.out.println($wrappee);
+		}
+	}		
+	
+	public void doSomethingWithSome(Client c) {
+		W(c).print();
+	}
+}
