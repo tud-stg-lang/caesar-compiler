7afb000993250c66cf21894642f8cbf427ba37de
gasiunas <gasiunas>
2004-11-02 15:04:45 +0000

    Test methods commented

--- src/org/caesarj/test/AspectDeploymentTests.java
+++ src/org/caesarj/test/AspectDeploymentTests.java
@@ -6,114 +6,143 @@ public class AspectDeploymentTests extends FjTestCase
 		super(name);
 	}
 	
+	/* Test dynamic deployment with inheritance */
 	public void testCaesarTestCase_0() throws Throwable {
 		compileAndRun("test100", "ADTestCase");
 	}
 
+	/* Test longer inner aspect inheritance sequence */
 	public void testCaesarTestCase_1() throws Throwable {
 		compileAndRun("test101", "ADTestCase");
 	}
 
+	/* Test static aspect deployment */
 	public void testCaesarTestCase_2() throws Throwable {
 		compileAndRun("test102", "ADTestCase");
 	}
 
+	/* Test abstract crosscutting classes, implementing abstract pointcuts (not supported) */
 	public void testCaesarTestCase_3() throws Throwable {
 		compileAndRun("test103", "ADTestCase");
 	}
 
+	/* Test precedence declaration for crosscutting and deployed classes */
 	public void testCaesarTestCase_4() throws Throwable {
 		compileAndRun("test104", "ADTestCase");
 	}
-		
+	
+	/* Test after returning, after throwing, usage of join point reflection */
 	public void testCaesarTestCase_5() throws Throwable {
 		compileAndRun("test105", "ADTestCase");
 	}
 
+	/* Test around advices */
 	public void testCaesarTestCase_6() throws Throwable {
 		compileAndRun("test106", "ADTestCase");
 	}
 
+	/* Test thread safety of deployment */
 	public void testCaesarTestCase_7() throws Throwable {
 		compileAndRun("test107", "ADTestCase");
 	}
 
+	/* Test thread safety of deployment */
 	public void testCaesarTestCase_9() throws Throwable {
 		compileAndRun("test109", "ADTestCase");
 	}
-     	
+     
+	/* Test statically deployed around advices */
 	public void testCaesarTestCase_10() throws Throwable {
 		compileAndRun("test110", "ADTestCase");
 	}
 	
+	/* Test static aspect with concrete pointcut inherits
+       from static abstract aspect with concrete advice. (not supported) */
 	public void testCaesarTestCase_11() throws Throwable {
 		compileAndRun("test111", "ADTestCase");
 	}
 
+	/* Test deployment of multiple instances with around advices */
 	public void testCaesarTestCase_12() throws Throwable {
 		compileAndRun("test112", "ADTestCase");
 	}
 	
+	/* Test using Caesar type system for crosscutting */
 	public void testCaesarTestCase_13() throws Throwable {
 		compileAndRun("test113", "ADTestCase");
 	}
 	
+	/* Test crosscutting outer joins */
 	public void testCaesarTestCase_14() throws Throwable {
 		compileAndRun("test114", "ADTestCase");
 	}
 	
+	/* Test crosscuts in mixins */
 	public void testCaesarTestCase_15() throws Throwable {
 		compileAndRun("test115", "ADTestCase");
 	}
 	
+	/* Test if mixins are weaved */
 	public void testCaesarTestCase_16() throws Throwable {
 		compileAndRun("test116", "ADTestCase");
 	}
 
+	/* Test conditional pointcuts (not supported yet) */
 	public void testCaesarTestCase_17() throws Throwable {
 		compileAndRun("test117", "ADTestCase");
 	}
 	
+	/* Advices on inherited pointcuts */
 	public void testCaesarTestCase_18() throws Throwable {
 		compileAndRun("test118", "ADTestCase");
 	}
 	
+	/* Test deploy block robustness */
 	public void testCaesarTestCase_19() throws Throwable {
 		compileAndRun("test119", "ADTestCase");
 	}
 	
+	/* Test deployment of multiple objects on inherited registries */
 	public void testCaesarTestCase_20() throws Throwable {
 		compileAndRun("test120", "ADTestCase");
 	}
 	
+	/* Test exception softening (not supported yet) */
 	public void testCaesarTestCase_21() throws Throwable {
 		compileAndRun("test121", "ADTestCase");
 	}
 
+	/* Test privileged access. (not supported yet) */
 	public void testCaesarTestCase_22() throws Throwable {
 		compileAndRun("test122", "ADTestCase");
 	}
 	
+	/* Test multi-instance around calls */
 	public void testCaesarTestCase_23() throws Throwable {
 		compileAndRun("test123", "ADTestCase");
 	}
-		
+	
+	/* Test wrapping primitive types in around */
 	public void testCaesarTestCase_24() throws Throwable {
 		compileAndRun("test124", "ADTestCase");
 	}
 	
+	/* Test passing join point reflection info */
 	public void testCaesarTestCase_25() throws Throwable {
 		compileAndRun("test125", "ADTestCase");
 	}
 	
+	/* Test local deployment */
 	public void testCaesarTestCase_26() throws Throwable {
 		compileAndRun("test126", "ADTestCase");
 	}
 	
+	/* Test cflow pointcuts */
 	public void testCaesarTestCase_27() throws Throwable {
 		compileAndRun("test127", "ADTestCase");
 	}
 	
+	/* Test cross-thread cflow pointcuts */
 	public void testCaesarTestCase_28() throws Throwable {
 		compileAndRun("test128", "ADTestCase");
 	}
--- src/org/caesarj/test/VirtualClassesTests.java
+++ src/org/caesarj/test/VirtualClassesTests.java
@@ -6,18 +6,22 @@ public class VirtualClassesTests extends FjTestCase {
 		super(name);
 	}
 
+	/* Test factory methods late bound new */
 	public void testCaesarTestCase_0() throws Throwable {
         compileAndRun("test0", "VCTestCase");
     }
 
+	/* Test implements clause for cclass. */
     public void testCaesarTestCase_1() throws Throwable {
         compileAndRun("test1", "VCTestCase");
     }
 
+    /* Test &-Operator and linearization (with graphs) */
     public void testCaesarTestCase_2() throws Throwable {
         compileAndRun("test2", "VCTestCase");
     }
 
+    /* Test &-Operator and linearization (with graphs) */
     public void testCaesarTestCase_3() throws Throwable {
         compileAndRun("test3", "VCTestCase");
     }
@@ -46,138 +50,172 @@ public class VirtualClassesTests extends FjTestCase {
         compileAndRun("test9", "VCTestCase");
     }
 	
+	/*  Test factory methods of outer classes. */
 	public void testCaesarTestCase_10() throws Throwable {
         compileAndRun("test10", "VCTestCase");
     }
 
+	/* Inner classes of inner classes. */
 	public void testCaesarTestCase_11() throws Throwable {
         compileAndRun("test11", "VCTestCase");
     }
 
+	/* Test factory methods of inner classes. */
 	public void testCaesarTestCase_12() throws Throwable {
         compileAndRun("test12", "VCTestCase");
     }
 
+	/* Test inherited methods. */
 	public void testCaesarTestCase_13() throws Throwable {
         compileAndRun("test13", "VCTestCase");
     }
 
+	/* Test super calls. */
 	public void testCaesarTestCase_14() throws Throwable {
         compileAndRun("test14", "VCTestCase");
     }
 
+	/* Test super calls using extends for furtherbindings. */
     public void testCaesarTestCase_14b() throws Throwable {
         compileAndRun("test14b", "VCTestCase");
     }
 
+    /* Test long inheritance sequence. */
 	public void testCaesarTestCase_15() throws Throwable {
         compileAndRun("test15", "VCTestCase");
     }
 	
+	/* Test state inheritance. */
 	public void testCaesarTestCase_16() throws Throwable {
         compileAndRun("test16", "VCTestCase");
     }
 	
+	/* Test extending deep classes relationships. */
 	public void testCaesarTestCase_17() throws Throwable {
         compileAndRun("test17", "VCTestCase");
     }
 	
+	/* Test multiple inheritance of methods. */
 	public void testCaesarTestCase_18() throws Throwable {
         compileAndRun("test18", "VCTestCase");
     }
 
+	/* Test multiple inheritance of state. */
 	public void testCaesarTestCase_19() throws Throwable {
         compileAndRun("test19", "VCTestCase");
     }
 
+	/* Test joins linearization of state. */
 	public void testCaesarTestCase_20() throws Throwable {
         compileAndRun("test20", "VCTestCase");
     }
     
+	/* Test joining extensions of state. */
     public void testCaesarTestCase_21() throws Throwable {
         compileAndRun("test21", "VCTestCase");
     }    
 	
+    /* Test polymorphism of joined classes. */
 	public void testCaesarTestCase_22() throws Throwable {
         compileAndRun("test22", "VCTestCase");
     }    
     	
+	/* Test join inner classes. */
 	public void testCaesarTestCase_23() throws Throwable {
         compileAndRun("test23", "VCTestCase");
     }
 	
+	/* Test arrays on Caesar classes. */
 	public void testCaesarTestCase_24() throws Throwable {
         compileAndRun("test24", "VCTestCase");
     }
 	
+	/* Test default constructors. */
 	public void testCaesarTestCase_25() throws Throwable {
         compileAndRun("test25", "VCTestCase");
     }
 
+	/* Test extends for furtherbindings. */
 	public void testCaesarTestCase_26() throws Throwable {
         compileAndRun("test26", "VCTestCase");
     }
 	
+	/* Test multiple outer joins. */
 	public void testCaesarTestCase_27() throws Throwable {
         compileAndRun("test27", "VCTestCase");
     }
 
+	/* Test merging class with multiple parents. */
 	public void testCaesarTestCase_28() throws Throwable {
         compileAndRun("test28", "VCTestCase");
     }
 	
+	/* Test introducing new inheritance. */
 	public void testCaesarTestCase_29() throws Throwable {
         compileAndRun("test29", "VCTestCase");
     }
 
+	/* Test merging class hierarchies. */
 	public void testCaesarTestCase_30() throws Throwable {
         compileAndRun("test30", "VCTestCase");
     }
     
+	/* Test outer class creation. */
     public void testCaesarTestCase_31() throws Throwable {
         compileAndRun("test31", "VCTestCase");
     }
     
+    /* Test introducing new inheritance. */
     public void testCaesarTestCase_32() throws Throwable {
         compileAndRun("test32", "VCTestCase");
     }
     
+    /* Test virtual class scoping */
     public void testCaesarTestCase_33() throws Throwable {
         compileAndRun("test33", "VCTestCase");
     }
 
+    /* Test virtual class scoping with deeper nesting */
     public void testCaesarTestCase_33b() throws Throwable {
         compileAndRun("test33b", "VCTestCase");
     }
 
+    /* Test mixin factory methods. */
 	public void testCaesarTestCase_34() throws Throwable {
         compileAndRun("test34", "VCTestCase");
     }
 	
+	/* Test Object methods through Caesar interfaces. */
 	public void testCaesarTestCase_35() throws Throwable {
         compileAndRun("test35", "VCTestCase");
     }
 	
+	/* Test inheritance cross package boundaries */
 	public void testCaesarTestCase_36() throws Throwable {
         compileAndRun("test36", "VCTestCase");
     }
 	
+	/* Test automatic casts */
 	public void testCaesarTestCase_37() throws Throwable {
         compileAndRun("test37", "VCTestCase");
     }
 	
+	/* Wrapper test */
 	public void testCaesarTestCase_38() throws Throwable {
         compileAndRun("test38", "VCTestCase");
     }
 	
+	/* Wrapper test */
 	public void testCaesarTestCase_39() throws Throwable {
         compileAndRun("test39", "VCTestCase");
     }
 	
+	/* Array Test (with board) */
 	public void testCaesarTestCase_40() throws Throwable {
         compileAndRun("test40", "VCTestCase");
     }
 	
+	/* Test subject oriented programming. */
 	public void testCaesarTestCase_99() throws Throwable {
         compileAndRun("test99", "VCTestCase");
     }
