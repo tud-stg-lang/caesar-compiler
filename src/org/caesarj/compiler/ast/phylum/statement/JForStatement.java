da5a01c35fa08b2e0d229451012d7314ef570ab6
aracic <aracic>
2004-11-15 15:54:36 +0000

    null checks in recurse

--- src/org/caesarj/compiler/ast/phylum/statement/JForStatement.java
+++ src/org/caesarj/compiler/ast/phylum/statement/JForStatement.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JForStatement.java,v 1.2 2004-09-06 13:31:34 aracic Exp $
+ * $Id: JForStatement.java,v 1.3 2004-11-15 15:54:36 aracic Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.statement;
@@ -155,9 +155,9 @@ public class JForStatement extends JLoopStatement {
    * @param	p		the visitor
    */
   public void recurse(IVisitor s) {
-    init.accept(s);
-    cond.accept(s);
-    incr.accept(s);
+    if(init != null) init.accept(s);
+    if(cond != null) cond.accept(s);
+    if(incr != null) incr.accept(s);
     body.accept(s);
   }
 
