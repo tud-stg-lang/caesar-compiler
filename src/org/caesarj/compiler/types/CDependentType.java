97b1fd5b377f44ebc67879f88f95d5465f2bed1a
aracic <aracic>
2004-11-19 13:03:24 +0000

    *** empty log message ***

--- src/org/caesarj/compiler/types/CDependentType.java
+++ src/org/caesarj/compiler/types/CDependentType.java
@@ -1,55 +1,37 @@
 package org.caesarj.compiler.types;
 
+import org.caesarj.compiler.ast.phylum.JPhylum;
+import org.caesarj.compiler.ast.phylum.expression.JExpression;
 import org.caesarj.compiler.context.CTypeContext;
 import org.caesarj.compiler.export.CClass;
-import org.caesarj.compiler.export.CMember;
 
 
 /**
  * Dependent Types, e.g.: 
- * final G g; g.N n; 
+ * final ctx(1).G g; ctx(0).g.N n; 
  * 
  * @author Ivica Aracic
  */
 public class CDependentType extends CReferenceType {
     
-    private CMember[] pos;
-    private CType staticType;
-    private String[] path;
-    private int k = 0;
+    private JPhylum[] pos; 	   	/** determines the position in the ast 
+                                    (starting with the class) */
     
-    public CDependentType(CMember[] pos, String[] path, CType staticType) {
+    private CType plainType;    /** static type of the */
+    
+    private JExpression family; /** family expression */
+    
+    private int k = 0;          /** ctx(k); determines how many steps to go out 
+                                    of the current context */
+    
+    public CDependentType(JPhylum[] pos, JExpression family, CType staticType) {
         this.pos = pos;
-        this.path = path;
-        this.staticType = staticType;
+        this.family = family;
+        this.plainType = staticType;
     }
     
     public CClass getCClass() {
-        return staticType.getCClass();
-    }
-    
-    protected boolean isPosEqual(CMember[] otherPos) {
-        if(pos.length != otherPos.length)
-            return false;
-        
-        for (int i = 0; i < otherPos.length; i++) {
-            if( pos[i] != otherPos[i] ) 
-                return false;
-        }
-        
-        return true;
-    }
-    
-    protected boolean isPathEqual(String[] otherPath) {
-        if(path.length != otherPath.length)
-            return false;
-        
-        for (int i = 0; i < otherPath.length; i++) {
-            if( !path[i].equals(otherPath[i]) ) 
-                return false;
-        }
-        
-        return true;
+        return plainType.getCClass();
     }
     
     public boolean isAssignableTo(CTypeContext context, CType dest) {
@@ -57,12 +39,8 @@ public class CDependentType extends CReferenceType {
         if(dest instanceof CDependentType) {
             CDependentType other = (CDependentType)dest;
             
-            if( staticType.isAssignableTo(context, other.getStaticType()) ) {
-                if(isPosEqual(other.pos)) {
-                    if(isPathEqual(other.path)) {
-                        return true;
-                    }                
-                }
+            if( plainType.isAssignableTo(context, other.getPlainType()) ) {
+                return true;
             }
         }
         
@@ -71,7 +49,7 @@ public class CDependentType extends CReferenceType {
     
 
     public boolean isChecked() {
-        return staticType.isChecked();
+        return plainType.isChecked();
     }
 
     // not castable for now
@@ -79,26 +57,19 @@ public class CDependentType extends CReferenceType {
         return false;
     }
     
-    public CMember[] getPos() {
-        return pos;
-    }
     
-    public String[] getPath() {
-        return path;
-    }
     
-    public CType getStaticType() {
-        return staticType;
+    public CType getPlainType() {
+        return plainType;
     }
     
     public String toString() {
         StringBuffer res = new StringBuffer();
-        for (int i = 0; i < path.length; i++) {            
-            res.append(path[i]);
-            res.append('.');        
-        }
-        
-        res.append(staticType);
+                
+        res.append("ctx("+k+""+").");
+        res.append(family);
+        res.append(".");
+        res.append(plainType);
         
         return res.toString();        
     }
