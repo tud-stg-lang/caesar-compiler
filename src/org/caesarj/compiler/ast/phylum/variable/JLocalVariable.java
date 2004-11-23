01b217b0baed99298dd67c31badcc2933d08e557
aracic <aracic>
2004-11-23 18:28:56 +0000

    recurse: check initialisating expression before recursing into

--- src/org/caesarj/compiler/ast/phylum/variable/JLocalVariable.java
+++ src/org/caesarj/compiler/ast/phylum/variable/JLocalVariable.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JLocalVariable.java,v 1.2 2004-09-06 13:31:34 aracic Exp $
+ * $Id: JLocalVariable.java,v 1.3 2004-11-23 18:28:56 aracic Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.variable;
@@ -218,7 +218,7 @@ public abstract class JLocalVariable extends JPhylum {
   }
 
   public void recurse(IVisitor s) {
-    expr.accept(s);
+    if(expr != null) expr.accept(s);
   }
   
   // ----------------------------------------------------------------------
