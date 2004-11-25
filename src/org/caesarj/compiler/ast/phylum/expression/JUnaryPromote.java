873ba5129c0b1e776f7b83e6c3f38602713ce8df
aracic <aracic>
2004-11-25 11:30:20 +0000

    getter added

--- src/org/caesarj/compiler/ast/phylum/expression/JUnaryPromote.java
+++ src/org/caesarj/compiler/ast/phylum/expression/JUnaryPromote.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JUnaryPromote.java,v 1.2 2004-09-06 13:31:35 aracic Exp $
+ * $Id: JUnaryPromote.java,v 1.3 2004-11-25 11:30:20 aracic Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.expression;
@@ -70,6 +70,10 @@ public class JUnaryPromote extends JExpression {
     return type;
   }
 
+  public JExpression getExpression() {
+      return expr;
+  }
+  
   // ----------------------------------------------------------------------
   // SEMANTIC ANALYSIS
   // ----------------------------------------------------------------------
