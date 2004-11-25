70bc690ba0508433731e8424b60af2205ad6ae27
aracic <aracic>
2004-11-25 11:30:16 +0000

    check prefix before recursion

--- src/org/caesarj/compiler/ast/phylum/expression/JSuperExpression.java
+++ src/org/caesarj/compiler/ast/phylum/expression/JSuperExpression.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: JSuperExpression.java,v 1.2 2004-09-06 13:31:35 aracic Exp $
+ * $Id: JSuperExpression.java,v 1.3 2004-11-25 11:30:16 aracic Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.expression;
@@ -175,7 +175,7 @@ public class JSuperExpression extends JExpression {
   }
 
   public void recurse(IVisitor s) {
-    prefix.accept(s);
+    if(prefix != null) prefix.accept(s);
   }
   
   // ----------------------------------------------------------------------
