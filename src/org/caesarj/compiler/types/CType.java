ca7a1d9cc88047046045f4de013b902a8244d396
klose <klose>
2005-01-13 18:20:56 +0000

    added getDefDepth

--- src/org/caesarj/compiler/types/CType.java
+++ src/org/caesarj/compiler/types/CType.java
@@ -15,12 +15,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: CType.java,v 1.5 2004-11-24 11:44:34 klose Exp $
+ * $Id: CType.java,v 1.6 2005-01-13 18:20:56 klose Exp $
  */
 
 package org.caesarj.compiler.types;
 
 import org.caesarj.compiler.constants.Constants;
+import org.caesarj.compiler.context.CClassContext;
+import org.caesarj.compiler.context.CContext;
 import org.caesarj.compiler.context.CTypeContext;
 import org.caesarj.compiler.export.CClass;
 import org.caesarj.util.InconsistencyException;
@@ -33,6 +35,37 @@ import org.caesarj.util.Utils;
  */
 public abstract class CType extends Utils implements Constants {
 
+    /**
+     * Calculate the number of steps to the outer context needed to find
+     * this type.
+     * @param in The context to search in
+     * @return The number of steps
+     */
+    public int getDefDepth(CContext in){
+        int k = 0;	
+        CClass myType = getCClass();
+        
+        // find class context
+        while ( !(in instanceof CClassContext) ){
+            k++;
+            in = in.getParentContext();
+        }
+        
+        CClassContext classContext = (CClassContext) in;
+        CClass 	ctx = classContext.getCClass(),
+        		parent = myType.getOwner();
+        
+        while( ctx != parent ){
+            if (ctx == null){
+                throw new InconsistencyException( ""+myType+" cannot be found in "+in);
+            }
+            ctx = ctx.getOwner();
+            k++;
+        }
+        
+        return k;
+    }
+    
   // ----------------------------------------------------------------------
   // CONSTRUCTORS
   // ----------------------------------------------------------------------
