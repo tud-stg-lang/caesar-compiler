32f2998d9c566a6c6ff47f1b8337e6e4eb41a42d
klose <klose>
2005-01-18 13:15:19 +0000

    removed dead code

--- src/org/caesarj/compiler/types/CDependentType.java
+++ src/org/caesarj/compiler/types/CDependentType.java
@@ -22,8 +22,6 @@ public class CDependentType extends CReferenceType {
     private int k = 0;          /** ctx(k); determines how many steps to go out 
                                     of the current context */
               
-    
-///    public CDependentType(CClassContext pos, int k, JFieldAccessExpression family, CType staticType) {
     public CDependentType(CContext ctx, int k, JExpression family, CType staticType) {
         setDefCtx(ctx);
         this.k = k;
@@ -32,10 +30,6 @@ public class CDependentType extends CReferenceType {
         System.out.println("new CDependentType: k="+k);
     }
     
-    /**
-     *
-     */
-
     public Path getPath() {
         return Path.createFrom(defCtx, family);
     }
@@ -43,31 +37,6 @@ public class CDependentType extends CReferenceType {
     public CClass getCClass() {
         return plainType.getCClass();
     }
-    
-    /*
-    public String makePath() {
-        StringBuffer res = new StringBuffer();
-        res.append("ctx("+k+")");
-        if(family != null) {
-            JFieldAccessExpression e = family;
-            LinkedList l = new LinkedList();
-            while(e != null) {
-                l.add(0, e.getIdent());
-                if(e.getPrefix() instanceof JFieldAccessExpression) {
-                    e = (JFieldAccessExpression)e.getPrefix();
-                }
-                else {
-                    e = null;
-                }
-            }
-            
-            for (Iterator it = l.iterator(); it.hasNext();) {
-                res.append("."+it.next());
-            }
-        }
-        return res.toString();
-    }     
-    */
     
     /**
      * simply check the plain type here
@@ -80,24 +49,6 @@ public class CDependentType extends CReferenceType {
             
             // check if plain types are subtypes
             if( plainType.isAssignableTo(context, other.plainType) ) {
-                
-                CContext ctx = (CContext) context;
-                
-                // TODO create static objects and compare
-                CDependentType rightType = (CDependentType) dest;
-                
-                /*
-                Path 	rightPath  = Path.createFrom(ctx, rightType.getFamily() ),
-                		leftPath   = Path.createFrom(ctx, this.getFamily() );
-                */
-               
-                /*
-                StaticObject 	rightSO = rightPath.type(ctx),
-                				leftSO = leftPath.type(ctx);
-                                
-                return leftSO.hasSameFamiliy(rightSO);
-                */
-                
                 return true;
             }
         }
