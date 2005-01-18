cd957d5b8f3464404da996bb8faf41b6c1b7af50
klose <klose>
2005-01-18 11:23:30 +0000

    *** empty log message ***

--- src/org/caesarj/compiler/family/Path.java
+++ src/org/caesarj/compiler/family/Path.java
@@ -45,122 +45,23 @@ public abstract class Path {
     public CReferenceType getType() {
         return type;
     }
+
+    /**
+     * Calculate <code>k</code> for an expression in a given context.
+     * @param contextClass The context in which the expression should be analysed
+     * @param expr	The expression containing the path
+     */
+    public static int calcK(CContext context, JExpression expr){
+        Path p = createFrom(context, expr);
+        return ((ContextExpression)p.getHead()).getK(); 
+    }
+    
     
     /**
      * Create a path from an expression.
      * @param contextClass The context in which the expression should be analysed
      * @param expr	The expression containing the path
      */
-    public static int calcK(CContext context, JExpression expr){
-    	
-        List 	l = new LinkedList();	// stores the field access identifiers (Strings) 
-        int 	k =0;
-        JExpression tmp = expr;
-        
-        boolean done=false;
-        
-        while (!done){
-            if (tmp instanceof JFieldAccessExpression){
-                // if tmp is a field-access...
-                JFieldAccessExpression fa = (JFieldAccessExpression)tmp;
-                if ( fa.getIdent().equals(Constants.JAV_OUTER_THIS) ){
-                    // ... if it is this$0.xxx then increase k ...
-                    context = context.getParentContext();
-                    k++;
-                } else {
-                    // ... else add identifier to path.
-                    l.add(0, fa.getIdent());
-                }
-                tmp = fa.getPrefix();
-            
-            }
-            else if (tmp instanceof JOwnerExpression) {
-                while (! (context instanceof CClassContext)){
-                    context = context.getParentContext();
-                    k++;
-                }
-                CClass owner = ((JOwnerExpression)tmp).getSelf();
-                CClass current = context.getClassContext().getCClass();
-                while(current != owner) {
-                    if(current == null) throw new InconsistencyException();
-                    current = current.getOwner();
-                    k++;
-                }
-                done = true;
-            }
-            else if (tmp instanceof JThisExpression ){
-                done = true;
-            } 
-            else if (tmp instanceof JLocalVariableExpression){
-                
-                JLocalVariableExpression local = (JLocalVariableExpression) tmp;
-                JLocalVariable var = local.getVariable();
-                if (var instanceof JFormalParameter){
-                    // find next MethodContext
-                    while (! (context instanceof CMethodContext)){
-                        context = context.getParentContext();
-                        k++;
-                    }
-                } else if (var instanceof JVariableDefinition){
-                    // find next block-context that declares this variable
-                    boolean found = false;
-                    do{
-                        if (! (context instanceof CBlockContext)){
-                            throw new RuntimeException("Cannot find "+var.getIdent());
-                        }
-                        CBlockContext block = (CBlockContext)context;
-                        if (block.containsVariable(var.getIdent())){
-                            found = true;
-                        } else {
-                            context = context.getParentContext();
-                            k++;
-                        }
-                    } while (!found);
-                    done = true;
-                }
-                
-            } else if (tmp instanceof JMethodCallExpression){
-                JMethodCallExpression me = (JMethodCallExpression) tmp;
-                if (me.getIdent().startsWith(Constants.JAV_ACCESSOR) ){
-                    CType type = me.getType(null);
-                    CClass clazz = type.getCClass();
-                    // ... find first class context ... 
-                    while (!(context instanceof CClassContext)){
-                        context = context.getParentContext();
-                        k++;
-                    }
-                    // ... and search for the correct outer class.
-                    while ( ((CClassContext)context).getCClass() != clazz ){
-                        context = context.getParentContext();
-                        k++;
-                        if ( context == null || !(context instanceof CClassContext) ){
-                            throw new InconsistencyException("accessor method does not return outer class");
-                        }
-                    }
-                    done = true;
-                } else {
-                    throw new InconsistencyException("Path can not include method calls");
-                }
-            } else {
-                throw new InconsistencyException("Illegal expression in dependent type path: "+tmp);
-            }
-        }
-        
-        
-        return k;
-        // Construct path        
-        //Path result = new ContextExpression(k);
-        
-        /*
-        for (Iterator iter = l.iterator(); iter.hasNext();) {
-            String field = (String) iter.next();
-            result = new FieldAccess(result, field);            
-        }        
-        return result;
-        */
-    }
-    
-    
     public static Path createFrom(CContext context, JExpression expr){    	
         List 	l1 = new LinkedList();	// stores the field access identifiers (Strings)
         List 	l2 = new LinkedList();	// stores the field access identifiers (Strings)
