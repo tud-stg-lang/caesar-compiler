bc751d94f4b329af6d79625005e13bd3bf9113aa
aracic <aracic>
2005-01-25 16:48:49 +0000

    perl script for replacing headers in source files

---
+++ scripts/replace-headers.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# This script replaces the source file headers 
+
+$files = `find ../../org.caesarj.ui/src/* -name *.java`;
+
+@files = split(/\n/, $files);
+
+$header = `cat header.txt`;
+
+foreach $f (@files) {	
+	open (IN, $f);
+	open (OUT, ">$f.tmp");	
+	
+	$_ = "";
+	while($line = <IN>)  {
+		$_ .= $line;
+	
+	}
+	close(IN);
+
+	/package org\.caesarj\..+;(\n|\r\n)/;
+
+	print OUT $header;	
+	print OUT $&;
+	print OUT $';
+		
+	close(OUT);
+	`mv $f.tmp $f`;
+}
