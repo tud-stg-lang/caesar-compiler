10d1a33fd3d1c81dfa627a4d098590dbf2332890
aracic <aracic>
2005-02-07 18:24:22 +0000

    showcase for type checking method calls

--- test/TypeSystemTests/typesystest09/TypeSysTestCase.java
+++ test/TypeSystemTests/typesystest09/TypeSysTestCase.java
@@ -11,37 +11,57 @@ public class TypeSysTestCase {
     final G g2 = new G();
     
     final g1.X x1 = g1.new X();
-    final g1.X x2 = g2.new X();
+    final g2.X x2 = g2.new X();
     
     x1.N n1 = x1.new N();
     x2.N n2 = x2.new N();
     
-    
+
+    B b = new B();
+    b.g.X x = null;
     
 	public void foo() {	    
 	    bar(g1, x1, n1);
-	    bar(x2); // this should fail
-	}
-	
-	public void bar(final G graph, graph.X x, x.N n) {
-	    // ...
-	}
-
-	/* Signature problem with Java.. 
-	 * g2.X and g1.X are both mapped to G.X 
-	 */
-	//public void bar(g2.X x) {}
-	
-	
-	public void bar(g1.X x) {
-	    // ...
+	    bar(x1); 
+	    
+	    b.bar(x);
+	    //b.x.bar(x);
+	    
+	    g1.X x;
+	    x = bar();
 	}
 
+	public g1.X bar() {
+	    return null;
+	}	
+	
+	/*
+	public g.X bar(final G g) {
+	    return null;
+	}
+	*/
+	
+	
+	public void bar(final G graph, graph.X x, x.N n) {}
+
+	public void bar(g1.X x) {}
+
+}
+
+public cclass B {
+    public G g = new G();
+    
+    public void bar(g.X x) {
+    }
+    
+    public X x = new X();
+    public cclass X {
+        public void bar(g.X x) { }
+    }
 }
 
 public cclass G {
     public cclass X {
-        public cclass N {
-        }
+        public cclass N {}
     }    
 }
