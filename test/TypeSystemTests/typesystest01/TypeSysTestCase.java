5b0e54896a95ec24e841e77951124fb219f295cf
aracic <aracic>
2005-02-16 16:35:23 +0000

    not valid

--- test/TypeSystemTests/typesystest01/TypeSysTestCase.java
+++ test/TypeSystemTests/typesystest01/TypeSysTestCase.java
@@ -7,39 +7,7 @@ package generated.typesystest01;
  */
 public class TypeSysTestCase {
 
-	public X x = new X();		
-	
-	public final G g = new G();	
-	
-	public g.N n;		
 		
-	class X {
-	    Z z = new Z();
-	    class Z {
-	        TypeSysTestCase.X.Z.Y y = new Y();
-	        class Y {
-	            public g.N n;
-			    
-			    public void test() {
-			        /*
-			        TypeSysTestCase.access$000( 
-			            X.access$000( X.Z.access$000(this$0) )  
-		            ).toString();
-		            
-		            g.toString();
-			        */
-			        n = x.z.y.n;       
-			    }
-		    }
-	    }
-	}	
-	
-	public void test() {	    
-	    g.N nn = null;	    	    
-	    {
-	        x.z.y.n = nn;
-	    }	 
-	}	
 }
 
 public cclass G {
