0b8adc197aa8c6b60a04b5e4e2dd427e831057b7
aracic <aracic>
2005-02-16 13:25:14 +0000

    *** empty log message ***

--- test/TypeSystemTests/typesystest10/TypeSysTestCase.java
+++ test/TypeSystemTests/typesystest10/TypeSysTestCase.java
@@ -13,33 +13,23 @@ import org.caesarj.runtime.*;
  */
 public class TypeSysTestCase extends TestCase {
     
+    final B b = new B();
+    
     public TypeSysTestCase() {
 		super("test");
-	}
-
-    /** this method is going to land in caesar's runtime lib */
-    public static Object cast(Object o1, CaesarObject o2) {
-		if(o1 != null) {
-			if(o1 instanceof CaesarObject) {
-				if(!((CaesarObject)o1).familyEquals( o2 )) {
-					throw new RuntimeException();
-				}
-			}
-			else if(!(o1 instanceof Object)) {
-				throw new RuntimeException();
-			}
-		}
-		return o2;
-	}	
-
-	final B b = new B();
+	}   
 			
 	public void test() {
 	    System.out.println("TypeSysTestCase: start");
-	    final b.g1.X x = b.x1;
+	    final b.g1.X x = b.x1;	    
 	    
-	    x.N n;
+	    x.N n;	    
+	    
+	    n = null;
 	    n = (x.N)b.n1;
+	    
+	    n = null;	    
+	    //n = (x.N)b.n2; // should fail
 	    
 	    List l = new LinkedList();
 	    l.add(n);
@@ -50,14 +40,14 @@ public class TypeSysTestCase extends TestCase {
 }
 
 public cclass B {
-    public final G g1 = null;
-    public final G g2 = null;
+    public final G g1 = new G();
+    public final G g2 = new G();
     
-    public final g1.X x1 = null;
-    public final g2.X x2 = null;
+    public final g1.X x1 = g1.new X();
+    public final g2.X x2 = g2.new X();
     
-    public x1.N n1 = null;
-    public x2.N n2 = null;
+    public x1.N n1 = x1.new N();
+    public x2.N n2 = x2.new N();
 }
 
 public cclass G {
