a835d6975de7e846237e64914c5bae024828354b
meffert <meffert>
2005-05-31 08:53:41 +0000

    added parameter (JFormalParameter[]) to CSourceMethod to support parameters in localvariabletable (debugging)

--- src/org/caesarj/compiler/ast/phylum/declaration/CjAdviceDeclaration.java
+++ src/org/caesarj/compiler/ast/phylum/declaration/CjAdviceDeclaration.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * $Id: CjAdviceDeclaration.java,v 1.12 2005-04-15 12:42:09 gasiunas Exp $
+ * $Id: CjAdviceDeclaration.java,v 1.13 2005-05-31 08:53:41 meffert Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.declaration;
@@ -211,6 +211,7 @@ public class CjAdviceDeclaration
                 ACC_PUBLIC,
                 ident,
                 returnType,
+				parameters,
                 parameterTypes,
                 exceptions,
                 body,
--- src/org/caesarj/compiler/ast/phylum/declaration/JAccessorMethod.java
+++ src/org/caesarj/compiler/ast/phylum/declaration/JAccessorMethod.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * $Id: JAccessorMethod.java,v 1.6 2005-03-06 13:45:59 aracic Exp $
+ * $Id: JAccessorMethod.java,v 1.7 2005-05-31 08:57:32 meffert Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.declaration;
@@ -201,6 +201,7 @@ public class JAccessorMethod extends JMemberDeclaration {
                                ACC_STATIC,
                                ident,
                                returnType,
+                               parameters,
                                parameterTypes,
                                throwables,
                                false,
