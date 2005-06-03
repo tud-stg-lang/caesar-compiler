a70e9477e0b7f05fd188a136c56613c49a9685e8
klose <klose>
2005-06-03 08:12:35 +0000

    *** empty log message ***

--- src/org/caesarj/compiler/aspectj/CaesarBcelSourceContext.java
+++ src/org/caesarj/compiler/aspectj/CaesarBcelSourceContext.java
@@ -1,9 +1,28 @@
 /*
- * Created on 20.04.2005
+ * This source file is part of CaesarJ 
+ * For the latest info, see http://caesarj.org/
+ * 
+ * Copyright © 2003-2005 
+ * Darmstadt University of Technology, Software Technology Group
+ * Also see acknowledgements in readme.txt
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * $Id: CaesarBcelSourceContext.java,v 1.2 2005-06-03 08:12:35 klose Exp $
  */
+
 package org.caesarj.compiler.aspectj;
 
 import java.io.File;
@@ -18,8 +37,7 @@ import org.aspectj.weaver.bcel.BcelSourceContext;
 /**
  * @author vaidas
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * TODO [documentation]
  */
 public class CaesarBcelSourceContext extends BcelSourceContext {
 	
