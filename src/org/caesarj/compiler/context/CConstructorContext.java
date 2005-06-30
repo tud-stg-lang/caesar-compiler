3997beeacd527c55118fc90104ed92bd7a3b54d5
klose <klose>
2005-06-30 13:45:35 +0000

    *** empty log message ***

--- src/org/caesarj/compiler/context/CConstructorContext.java
+++ src/org/caesarj/compiler/context/CConstructorContext.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * $Id: CConstructorContext.java,v 1.4 2005-01-24 16:52:58 aracic Exp $
+ * $Id: CConstructorContext.java,v 1.5 2005-06-30 13:45:35 klose Exp $
  */
 
 package org.caesarj.compiler.context;
@@ -42,6 +42,10 @@ import org.caesarj.util.UnpositionedError;
  * @see CContext
  */
 public class CConstructorContext extends CMethodContext {
+
+    public String toString() {
+        return "Ctr-ctx"+ "<- "+parent;
+    }
 
   // ----------------------------------------------------------------------
   // CONSTRUCTORS
