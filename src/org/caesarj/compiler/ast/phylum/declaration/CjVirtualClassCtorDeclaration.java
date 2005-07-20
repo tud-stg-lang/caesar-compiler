63827685f74d3140c14c130c74766c5dd130b036
gasiunas <gasiunas>
2005-07-20 10:06:00 +0000

    constructor checks moved to earlier stage

--- src/org/caesarj/compiler/ast/phylum/declaration/CjVirtualClassCtorDeclaration.java
+++ src/org/caesarj/compiler/ast/phylum/declaration/CjVirtualClassCtorDeclaration.java
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * $Id: CjVirtualClassCtorDeclaration.java,v 1.5 2005-05-31 08:57:13 meffert Exp $
+ * $Id: CjVirtualClassCtorDeclaration.java,v 1.6 2005-07-20 10:06:00 gasiunas Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.declaration;
@@ -34,15 +34,9 @@ import org.caesarj.compiler.ast.phylum.statement.JEmptyStatement;
 import org.caesarj.compiler.ast.phylum.statement.JStatement;
 import org.caesarj.compiler.ast.phylum.variable.JFormalParameter;
 import org.caesarj.compiler.ast.phylum.variable.JLocalVariable;
-import org.caesarj.compiler.constants.CaesarMessages;
-import org.caesarj.compiler.constants.KjcMessages;
-import org.caesarj.compiler.context.CClassContext;
-import org.caesarj.compiler.export.CModifier;
-import org.caesarj.compiler.export.CSourceMethod;
 import org.caesarj.compiler.types.CReferenceType;
 import org.caesarj.compiler.types.CType;
 import org.caesarj.compiler.types.TypeFactory;
-import org.caesarj.util.PositionedError;
 import org.caesarj.util.TokenReference;
 
 /**
@@ -93,31 +87,6 @@ public class CjVirtualClassCtorDeclaration extends JConstructorDeclaration {
             } : new JStatement[] {
                 body
             }), null, null, factory);
-    }
-
-    public CSourceMethod checkInterface(CClassContext context)
-        throws PositionedError {
-
-        // check public
-        check(
-            context,
-            CModifier.contains(getModifiers(), ACC_PUBLIC),
-            CaesarMessages.CCLASS_CTOR_PUBLIC);
-
-        // IVICA: check ctor name
-        // this check is done in JConstructorDeclaration again
-        // here we tune the error message in order to avoid displaying _Impl
-        // suffix
-        check(
-            context,
-            ident == context.getCClass().getIdent(),
-            KjcMessages.CONSTRUCTOR_BAD_NAME,
-            "",
-            context.getCClass().getIdent().
-            	substring(0, context.getCClass().getIdent().length() - 5)
-    	);
-
-        return super.checkInterface(context);
     }
 
     private static JFormalParameter genFormalParameter(
