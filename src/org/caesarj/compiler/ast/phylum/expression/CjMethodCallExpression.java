77956d94cf2e40217db3e1c1dc84d7a463086a89
gasiunas <gasiunas>
2005-07-28 11:31:22 +0000

    Use cast utils

--- src/org/caesarj/compiler/ast/phylum/expression/CjMethodCallExpression.java
+++ src/org/caesarj/compiler/ast/phylum/expression/CjMethodCallExpression.java
@@ -20,16 +20,16 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * $Id: CjMethodCallExpression.java,v 1.13 2005-06-10 12:21:02 klose Exp $
+ * $Id: CjMethodCallExpression.java,v 1.14 2005-07-28 11:31:22 gasiunas Exp $
  */
 
 package org.caesarj.compiler.ast.phylum.expression;
 
 import org.caesarj.compiler.ast.visitor.IVisitor;
+import org.caesarj.compiler.cclass.CastUtils;
 import org.caesarj.compiler.constants.CaesarConstants;
 import org.caesarj.compiler.context.CExpressionContext;
 import org.caesarj.compiler.context.GenerationContext;
-import org.caesarj.compiler.export.CClass;
 import org.caesarj.compiler.types.CType;
 import org.caesarj.compiler.types.TypeFactory;
 import org.caesarj.util.InconsistencyException;
@@ -65,54 +65,21 @@ public class CjMethodCallExpression extends JExpression implements CaesarConstan
 
         // analyse expression
         expr = (JMethodCallExpression)expr.analyse(context);
-        
-        
+                
         // cast the return type
         CType returnType = expr.getType(factory);        
         
-        //TODO Fix this code when implementing cclass arrays
-        if(!returnType.isArrayType() && returnType.isClassType()) {
-            
-            CClass returnClass = returnType.getCClass();
-
-            if(returnClass.isMixinInterface()) {
-                
-                String contextClassName = null;
-                
-                CClass prefixClass = expr.getPrefixType().getCClass();
-                
-                if(prefixClass.isMixin()) {
-                    // in this case we have this or super;
-                    // in both cases the context class is the local class
-                    contextClassName = context.getClassContext().getCClass().convertToIfcQn();
-                }
-                else {
-                    contextClassName = prefixClass.getQualifiedName();
-                }
-                
-                String newReturnClassQn = 
-	                context.getEnvironment().getCaesarTypeSystem().
-	                	findInContextOf(
-                	        returnClass.getQualifiedName(),
-                	        contextClassName
-		                );
-                
-                if(newReturnClassQn != null) {
-		            CClass newReturnClass = 
-		                context.getClassReader().loadClass(
-		                    factory,
-		                    newReturnClassQn
-		                );
-		            
-		            JExpression res = new CjCastExpression(
-	                    getTokenReference(),
-	                    expr,
-	                    newReturnClass.getAbstractType()
-		            );
-		            
-		            return res.analyse(context);
-	            }
-            }
+        CType castType = 
+          CastUtils.instance().castFrom(
+              context, returnType, expr.getPrefixType().getCClass());
+      
+        if (castType != null) {
+            JExpression res = new CjCastExpression(
+                getTokenReference(),
+                expr,
+                castType
+            );
+            return res.analyse(context);
         }
         
         return expr;
