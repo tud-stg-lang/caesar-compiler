323112992dc5fe870066107857aceb28df92e593
thiago <thiago>
2005-09-19 08:44:27 +0000

    - created a registry for pointcuts which permits us to get more information about a pointcut when resolving it

--- src/org/caesarj/compiler/aspectj/CaesarPointcutScope.java
+++ src/org/caesarj/compiler/aspectj/CaesarPointcutScope.java
@@ -2,7 +2,7 @@
  * This source file is part of CaesarJ 
  * For the latest info, see http://caesarj.org/
  * 
- * Copyright © 2003-2005 
+ * Copyright ï¿½ 2003-2005 
  * Darmstadt University of Technology, Software Technology Group
  * Also see acknowledgements in readme.txt
  * 
@@ -24,9 +24,18 @@
 
 package org.caesarj.compiler.aspectj;
 
+import java.util.IdentityHashMap;
+
 import org.aspectj.weaver.IHasPosition;
 import org.aspectj.weaver.ResolvedTypeX;
+import org.aspectj.weaver.Shadow;
 import org.aspectj.weaver.TypeX;
+import org.aspectj.weaver.patterns.Pointcut;
+import org.aspectj.weaver.patterns.ReferencePointcut;
+import org.aspectj.weaver.patterns.TypePattern;
+import org.aspectj.weaver.patterns.WithinPointcut;
+import org.aspectj.weaver.patterns.WithincodePointcut;
+import org.caesarj.compiler.constants.CaesarConstants;
 import org.caesarj.compiler.context.FjClassContext;
 import org.caesarj.compiler.export.CClass;
 import org.caesarj.util.TokenReference;
@@ -39,6 +48,30 @@ import org.caesarj.util.TokenReference;
  *
  */
 public class CaesarPointcutScope extends CaesarScope {
+	
+	/**
+	 * A map between type patterns and pointcuts.
+	 * 
+	 * Note that it MUST be an Identity map, because two references
+	 * for similar type patterns would be the same and the last
+	 * pointcut would overwrite the others (for example, if ClsA appears
+	 * in 2 pointcuts, the second would overwrite).
+	 */
+	protected static IdentityHashMap map = new IdentityHashMap();
+	
+	/**
+	 * Registers the relation between the type pattern and the pointcut.
+	 * 
+	 * @param type
+	 * @param p
+	 */
+	public static void register(TypePattern type, Pointcut p) {
+		map.put(type, p);
+	}
+	
+	public static void resetRegister() {
+		map = new IdentityHashMap();
+	}
 	
     /**
      * 
@@ -59,27 +92,93 @@ public class CaesarPointcutScope extends CaesarScope {
 	 */
 	public TypeX lookupType(String typeName, IHasPosition location) {
 		
-		if (context.getTypeFactory().isPrimitive(typeName)) return TypeX.forName(typeName); 
+		// Primitive types are resolved directly
+		if (context.getTypeFactory().isPrimitive(typeName)) 
+			return TypeX.forName(typeName);
+		
+		// Copies the name object
+		String name = new String(typeName);
+		
+		// Check if the map has a pointcut for this location
+		if (map.containsKey(location)) {
+			
+			// Gets the pointcut for the location
+			Pointcut p = (Pointcut) map.get(location);
+			
+			// Kinded Pointcuts
+			if (p instanceof CaesarKindedPointcut) {
+		
+				CaesarKindedPointcut kp = (CaesarKindedPointcut) p;
 
-		CClass cclass = lookupClass(typeName);
-/*
-		// hack - extract information if subtypes included
-		boolean includeSubtypes = false;
-		if (location instanceof WildTypePattern) {
-			WildTypePattern pattern = (WildTypePattern) location;
-			includeSubtypes = pattern.isIncludeSubtypes();
-		}
-				
-		// If the lookup retrieves a crosscutting class, then its
-		// aspect registry should be returned instead.
-		// Hence do a lookup for the registry.
-		if (cclass != null && !includeSubtypes && cclass instanceof CCjIfcSourceClass) {
-		    CClass aspectType = ((CCjIfcSourceClass) cclass).getRegistryClass();
-			if (aspectType != null) {
-				cclass = aspectType;
+				// TODO - this is not working...
+				if (Shadow.MethodCall.equals(kp.getKind())) {
+					// I am not sure why, but we don't need to append Impl for
+					// this case. Actually, it doesn't work if we do
+					//name += CaesarConstants.IMPLEMENTATION_EXTENSION;
+				}
+				if (Shadow.MethodExecution.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.FieldGet.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.FieldSet.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.ConstructorCall.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.ConstructorExecution.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.PreInitialization.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.StaticInitialization.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				if (Shadow.Initialization.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				// TODO - I don't think it is needed
+				if (Shadow.ExceptionHandler.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+				// TODO - Still couldn't find a running example
+				if (Shadow.AdviceExecution.equals(kp.getKind())) {
+					name = convertToImplName(name);
+				}
+			}
+			
+			// Within Pointcuts
+			if (p instanceof WithinPointcut) {
+				name = convertToImplName(name);
+			}
+			
+			// WithinCode Pointcuts
+			// TODO - False. We have to test for constructors
+			if (p instanceof WithincodePointcut) {
+				WithincodePointcut kp = (WithincodePointcut) p;
+				name = convertToImplName(name);
+			}
+			
+			// Reference Pointcuts
+			if (p instanceof ReferencePointcut) {
+				ReferencePointcut kp = (ReferencePointcut) p;
+				name = convertToImplName(name) + "$Registry";
 			}
 		}
-*/
+		
+		CClass cclass = lookupClass(name);
+		
+		// If name was not found and it is different from typeName
+		// it means the class was a Java Class and not a CClass.
+		// So, try with the Java Type Name
+		if (cclass == null) {
+			cclass = lookupClass(typeName);
+		}
+
+		// Resolve the class
 		if (cclass == null) {
 			return ResolvedTypeX.MISSING;
 		} 
@@ -88,4 +187,19 @@ public class CaesarPointcutScope extends CaesarScope {
 			return world.resolve(cclass);
 		}
 	}
+	
+	/**
+	 * Converts a class name to an implementation name.
+	 * Examples:
+	 *   ClassA -> ClassA_Impl
+	 *   ClassA$ClassB -> ClassA_Impl$ClassB_Impl
+	 *   
+	 * @param name the class name
+	 * @return the name of its implementation class
+	 */
+    public String convertToImplName(String name) {        
+        return 
+        	name.replaceAll("\\$", CaesarConstants.IMPLEMENTATION_EXTENSION + "\\$") +
+        	CaesarConstants.IMPLEMENTATION_EXTENSION;
+    }
 }
